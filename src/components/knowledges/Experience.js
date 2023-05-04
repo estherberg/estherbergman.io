c49e97068ec4449ae4e6f3320f802c32b531dba5
unknown <estherbergman16@gmail.com>
2023-05-04 21:06:33 +0300

    ca va

--- src/components/knowledges/Experience.js
+++ src/components/knowledges/Experience.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React from "react";
 
 const Experience = () => {
-    return (
-        <div className="experience">
-            <h3>Experiences</h3>
-            <div className="exp-1">
-                <h4>Segula Technologies - Web Developer</h4>
-                <h5>January-February 2021</h5>
-                <p>Create code in TypeScript, Angular.</p><p>Using Bootstarp – CSS.</p><p>Database: Adding fields to a table (SQL).</p><p>File manipulation in C# on the site.</p>
-
-            </div>
-            <div className="exp-2">
-                <h4>SwissLife Asset Managers - Hostess</h4>
-                <h5>August 2019</h5>
-                <p>Reception of customers, telephone reception, management of badges.
-Reservation of meeting rooms, management of telephone messaging.
-Management of online reservations, ordering of equipment.
-Maintenance and request for intervention on the site.</p>
-
-            </div>
-        </div>
-    );
+  return (
+    <div className="experience">
+      <h3>Experiences</h3>
+      <div className="exp-1">
+        <h4>TOCnDix -- Mendix Developperr</h4>
+        <h5>2022-2023</h5>
+        <p>
+        ✓Developing secure and enterprise grade low code application using the
+          leading low code platform: Mendix , using JavaScript.
+        </p>
+        <p>✓In charge of the full application development
+process of an app sold on the Mendix marketplace ✓
+Design, data model creation, actual development,
+integration, deployment and maintenance.
+</p>
+        <p>✓Project managed with agile method.</p>
+       
+      </div>
+      <div className="exp-2">
+        <h4>Segula Technologies - Web Developer</h4>
+        <h5>2021</h5>
+        <h4>Segula Technologies - Web Developer</h4>
+        <h5>January-February 2021</h5>
+        <p>Create code in TypeScript, Angular.</p>
+        <p>Using Bootstarp – CSS.</p>
+        <p>Database: Adding fields to a table (SQL).</p>
+        <p>File manipulation in C# on the site.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Experience;
