bfd70f6993d4d55667fad60b21debda27d7cc93b
unknown <estherbergman16@gmail.com>
2023-05-22 13:55:16 +0300

    ca va

--- src/components/knowledges/Experience.js
+++ src/components/knowledges/Experience.js
@@ -8,15 +8,18 @@ const Experience = () => {
         <h4>TOCnDix -- Software Developper</h4>
         <h5>2022-2023</h5>
         <p>
-        ✓Developing secure and enterprise grade low code application using the
-          leading low code platform: Mendix , using JavaScript.
+        ✓Server side developement : Java
         </p>
-        <p>✓In charge of the full application development
-process of an app sold on the Mendix marketplace ✓
-Design, data model creation, actual development,
-integration, deployment and maintenance.
-</p>
+        <p>✓Experience designing and building
+complex single-page applications:
+ReactJs & CSS3.</p> 
+<p>✓Database management: PostgreSql</p>
+
         <p>✓Project managed with agile method.</p>
+        <p>✓Use of different tools: Github,Jira.</p>
+        <p>✓Design, data model creation, actual
+development, integration, deployment,
+maintenance and security.</p>
        
       </div>
       <div className="exp-2">
